d58b3972bb570cc99eda9db3692a598a6a810139
slypheonix05 <91560816+slypheonix05@users.noreply.github.com>
2022-03-18 00:32:57 +0800

    bootstrap

--- mysimpleweb/urls.py
+++ mysimpleweb/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp1 import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'myapp1'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.MyIndexView.as_view(), name="my_index_web"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
